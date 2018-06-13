42ba518101893a44adf53ee28738a015a92fa2a5
winderbinder <junior_19000@hotmail.com>
2018-06-12 21:58:45 -0600

    added a test environment and added variables for dynamic behaviour

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,43 @@
 console.log("app.js is running");
 
 // JSX - JavaScript XML
-var template = <p>This is JSX from app.js!</p>;
-var appRoot = document.getElementById('app');
+
+let app = {
+  title: "Indecision App!",
+  subTitle: "This is some info for you!",
+  options: ["One", "Two"]
+};
+
+let template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subTitle && <p>{app.subTitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+    <ol>
+      <li>list 1</li>
+      <li>list 2</li>
+    </ol>
+  </div>
+);
+
+let user = {
+  name: "Andrew",
+  age: 26,
+  location: "Philadelphia"
+};
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+let template2 = (
+  <div>
+    <h1>{user.name ? user.name : "Anonymous"}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
+let appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/playground/es6-let-const.js
@@ -0,0 +1,3 @@
+var nameVar = "Andrew";
+nameVar = "Mike";
+console.log("nameVar", nameVar);
