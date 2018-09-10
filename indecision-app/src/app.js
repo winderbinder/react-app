dc06a92b6f8af82f4a7f03ef396283a9ffb60bb7
winderbinder <junior_19000@hotmail.com>
2018-09-10 11:57:46 -0600

    added children to component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,5 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp options={["option 1", "option 2"]}/>, document.getElementById("app"));
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.children}
+      <p>footer</p>
+    </div>
+  );
+};
+
+ReactDOM.render((
+  <Layout>
+    <p>This is inline</p>
+  </Layout>
+), document.getElementById("app"));
 
