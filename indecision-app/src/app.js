9e551e63e7aa01c6d9b8e0cb53163667cf03e0db
winderbinder <junior_19000@hotmail.com>
2018-09-10 11:35:52 -0600

    refactored component code

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,28 +4,3 @@ import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp options={["option 1", "option 2"]}/>, document.getElementById("app"));
 
-class OldSyntax {
-  constructor() {
-    this.name = "Mike";
-    this.getGreeting = this.getGreeting.bind(this);
-  }
-  getGreeting() {
-    return `hi my name is ${this.name}.`;
-  }
-}
-
-const oldSyntax = new OldSyntax();
-const getOldGreeting = oldSyntax.getGreeting;
-console.log(getOldGreeting());
-
-class NewSyntax {
-  name = 'jen';
-  getGreeting = () => {
-    return `hi my name is ${this.name}.`;
-  }
-}
-
-const newSyntax = new NewSyntax();
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());
-
