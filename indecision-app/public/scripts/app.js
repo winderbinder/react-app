a48f18d32eec605369482acd1cb2230dc836afa1
winderbinder <junior_19000@hotmail.com>
2018-09-24 12:05:42 -0600

    created feature where the selected decision opens in a new window and can be closed

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -14,30 +14,4 @@ var _IndecisionApp2 = _interopRequireDefault(_IndecisionApp);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Layout = function Layout(props) {
-  return _react2.default.createElement(
-    'div',
-    null,
-    _react2.default.createElement(
-      'p',
-      null,
-      'header'
-    ),
-    props.children,
-    _react2.default.createElement(
-      'p',
-      null,
-      'footer'
-    )
-  );
-};
-
-_reactDom2.default.render(_react2.default.createElement(
-  Layout,
-  null,
-  _react2.default.createElement(
-    'p',
-    null,
-    'This is inline'
-  )
-), document.getElementById("app"));
+_reactDom2.default.render(_react2.default.createElement(_IndecisionApp2.default, { options: ["option 1", "option 2"] }), document.getElementById("app"));
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,19 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-const Layout = (props) => {
-  return (
-    <div>
-      <p>header</p>
-      {props.children}
-      <p>footer</p>
-    </div>
-  );
-};
-
-ReactDOM.render((
-  <Layout>
-    <p>This is inline</p>
-  </Layout>
-), document.getElementById("app"));
+ReactDOM.render(<IndecisionApp options={["option 1", "option 2"]}/>, document.getElementById("app"));
 
